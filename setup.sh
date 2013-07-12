8632b06cc5169e2e8a2de1dc63bbb5a6a9da38f8
Ahmed T.Nabarawy <iNabarawy@gmail.com>
2013-07-12 23:59:47 +0000

    added -y flag to automatically accept all prompts

--- setup.sh
+++ setup.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 cd $HOME
 
-sudo apt-get install mysql-server mysql-client
+sudo apt-get -y install mysql-server mysql-client
 
-sudo apt-get install apache2
+sudo apt-get -y install apache2
 
-sudo apt-get install php5
+sudo apt-get -y install php5
 
-sudo apt-get install libapache2-mod-php5
+sudo apt-get -y install libapache2-mod-php5
 
 sudo /etc/init.d/apache2 restart
 
 sudo apt-cache search php5
 
-sudo apt-get install php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl
+sudo apt-get install -y php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl
 
 sudo /etc/init.d/apache2 restart
 
 chmod 755 /var/www
 
 sudo echo "<?php
-phpinfo();
-?>" > /var/www/info.php 
-
-sudo apt-get install postfix
-sudo apt-get install phpmyadmin
+echo ("Welcome World ! This is an automated setup via iNabarawy quick server setup via https://github.com/iNabarawy/server-setup.git");
+?>" > /var/www/index.php 
+if [ -e /var/www/index.html ]; then 
+rm -rf /var/www/index.html
+fi
+sudo apt-get -y install postfix
+sudo apt-get -y install phpmyadmin
 
 if [ -e /usr/share/phpmyadmin/index.php ]; then
 sudo mv /usr/share/phpmyadmin /var/www/phpmyadmin
